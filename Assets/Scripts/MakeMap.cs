b0b3173c260427396b28925dd4b8820b440a5dff
wp <wangp@soulgame.com>
2018-05-25 09:55:31 +0800

    11

---
+++ Assets/Scripts/MakeMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MakeMap : MonoBehaviour {
+
+	public NavMeshData navMeshData;
+
+	void Awake(){
+		Instantiate (Resources.Load ("Walls"));
+		if (navMeshData) {
+//			NavMeshDataInstance dI = NavMesh.AddNavMeshData (navMeshData);
+//			dI.Remove ();
+		}
+
+		var surface = GetComponent<NavMeshSurface> ();
+		if (surface) {
+//			NavMesh.RemoveNavMeshData(
+			surface.BuildNavMesh ();
+			print ("MakeMap  BuildNavMesh");
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,12 @@ public class PlayerController : MonoBehaviour {
 
     public NavMeshAgent agent;
 	public Animator animator;
-	public NavMeshSurface surface;
 	public NavMeshData data;
 
-	void Awake(){
-//		agent.SetDestination(Vector3.zero);
-//		NavMeshHit hit;
-//		var ss = NavMesh.SamplePosition (Vector3.zero, out hit, 1.0f, NavMesh.AllAreas);
-	}
+	private NavMeshPath path;
 
-	void Start(){
-		Instantiate (Resources.Load ("Walls"));
-		surface.BuildNavMesh();
+	void Awake(){
+		path = new NavMeshPath();
 	}
     
     // Update is called once per frame
@@ -34,22 +28,64 @@ public class PlayerController : MonoBehaviour {
 				Debug.Log ("++++");
 				NavMeshHit h;
 
-				var sor = new Vector3 (hit.point.x, hit.point.y, hit.point.z);
-//				print(NavMesh.GetAreaCost(NavMesh.GetAreaFromName ("Walkable")));
-//				print(NavMesh.GetAreaCost(NavMesh.GetAreaFromName ("Jump")));
-//				print(NavMesh.GetAreaFromName ("Jump"));
+//				var sor = new Vector3 (hit.point.x, hit.point.y, hit.point.z);
+				print(NavMesh.GetAreaCost(NavMesh.GetAreaFromName ("Walkable")));
+				print(NavMesh.GetAreaCost(NavMesh.GetAreaFromName ("TestArea")));
+				print(NavMesh.GetAreaFromName ("TestArea"));
 //				print ("++++");
 
-				var ss = NavMesh.SamplePosition (sor,out h, 1f, (int)NavMesh.GetAreaCost(NavMesh.GetAreaFromName ("Walkable")));
-//				Resources.loa
-//				Debug.Log (h.distance);
-				Debug.Log (sor);
-				Debug.Log (ss);
-//				Debug.Log (hit.point);
-				agent.SetDestination(hit.point);
-				animator.SetBool ("IsRun", true);
+				var ss = NavMesh.SamplePosition (hit.point,out h, 1f, NavMesh.AllAreas);
+//				Debug.DrawLine(this.transform.position, h.position, Color.red, 100000);
+//				print (h.position);
+				print (hit.point);
+				print ("++++++samp   : " + ss);
+
+				NavMeshHit hit1;
+				if (NavMesh.FindClosestEdge (hit.point, out hit1, 123)) {
+					DrawCircle (hit.point, hit1.distance, Color.red);
+					Debug.DrawRay (hit1.position, Vector3.up, Color.red, 10000);
+				}
+//				agent.SetDestination(hit.point);
+//				animator.SetBool ("IsRun", true);
+
+				//寻找Path
+//				NavMesh.CalculatePath(transform.position, hit.point, NavMesh.AllAreas, path);
+//				for (int i = 0; i < path.corners.Length - 1; i++) {
+////					print (path.corners [i]);
+//					Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red, 100000);
+//				}
+
 
             }
         }
+
+//		NavMeshHit hit1;
+//		if (NavMesh.SamplePosition(transform.position, out hit1, 1.0f, NavMesh.AllAreas))
+//		{
+//			Debug.DrawRay (hit1.position, Vector3.up, Color.red, 10000);
+//			Debug.Log (hit1.distance);
+////			DrawCircle (transform.position, hit1.distance, Color.red);
+//		}
+
+		//获取距离边界的距离
+//		NavMeshHit hit1;
+//		if (NavMesh.FindClosestEdge (transform.position, out hit1, NavMesh.AllAreas)) {
+//			DrawCircle (transform.position, hit1.distance, Color.red);
+//			Debug.DrawRay (hit1.position, Vector3.up, Color.red, 10000);
+//		} else {
+//			print ("+++++FindClosestEdge false");
+//		}
     }
+		
+	void DrawCircle(Vector3 center, float radius, Color color)
+	{
+		Vector3 prevPos = center + new Vector3(radius, 0, 0);
+		for (int i = 0; i < 30; i++)
+		{
+			float angle = (float)(i + 1) / 30.0f * Mathf.PI * 2.0f;
+			Vector3 newPos = center + new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+			Debug.DrawLine(prevPos, newPos, color, 10000);
+			prevPos = newPos;
+		}
+	}
 }
